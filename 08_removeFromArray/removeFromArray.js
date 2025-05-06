ac12a62b2df57f7b5df04e9013255f7fb8d64e57
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-06 15:06:56 -0500

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...elementsToRemove) {
+    let newArray = []
+    nextElement:
+    for (let element of originalArray){
+        if (!elementsToRemove.includes(element)){
+            newArray.push(element)
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
