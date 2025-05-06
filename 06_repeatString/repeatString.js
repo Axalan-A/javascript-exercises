a55514d372822baf2d6040e39a0b94a0024ff2cd
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-06 12:57:52 -0500

    Complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    let repeatedString = "";
+    if (timesToRepeat >= 0){
+        for (let i = 0; i < timesToRepeat; i++){
+            repeatedString += stringToRepeat
+        }
+    } else {
+        repeatedString = "ERROR"
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
