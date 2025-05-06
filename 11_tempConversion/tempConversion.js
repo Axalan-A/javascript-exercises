6840390680ab59392343405c45a30d2d65b1afeb
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-06 16:02:39 -0500

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInF) {
+  let temperatureInC = (temperatureInF - 32) * (5/9)
+  temperatureInC = Math.round(temperatureInC * 10)/10
+  return temperatureInC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInC) {
+  let temperatureInF = (temperatureInC * (9/5) + 32)
+  temperatureInF = Math.round(temperatureInF * 10)/10
+  return temperatureInF
 };
 
 // Do not edit below this line
