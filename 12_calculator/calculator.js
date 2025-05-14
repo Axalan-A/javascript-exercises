1d9ce21efeb5a19066f061262024e666bf7eb033
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-13 22:19:52 -0500

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((prod, current) => prod * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1
+  for(let i = 2; i <= a; i++){
+    product = product*i
+  }
+  return product
 };
 
 // Do not edit below this line
