0cfdc5c7c3aff0a7a66d119e3a7c29728f70fb97
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-23 14:23:29 -0500

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let date = new Date()
+    let oldest = people.reduce(
+        (oldest, current) => ((((current.yearOfDeath === undefined) ? date.getFullYear() : current.yearOfDeath) - current.yearOfBirth) 
+        > ((((oldest.yearOfDeath === undefined) ? date.getFullYear() : oldest.yearOfDeath) - oldest.yearOfBirth))
+        ? current: oldest))
+    return oldest
 };
 
 // Do not edit below this line
